b9d39778199ffae4a442ac9e4775820eefeca226
Baral-Chief-of-Compliance <78692798+Baral-Chief-of-Compliance@users.noreply.github.com>
2022-12-01 10:19:59 +0300

    delete tes geopy

--- test geopy/main.py
+++
@@ -1,8 +0,0 @@
-from geopy import Yandex
-
-place = "Баумана                    38Б, Мурманск"
-location = Yandex(api_key='aaca613a-fe9d-47d2-95f3-cd1714593fff').geocode(place)
-
-print(location.address)
-print("долгота " + str(location.longitude))
-print("широта "+str(location.latitude))
